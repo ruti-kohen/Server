bf38f21b6c5d3c6ee98cf20b1031491231074c81
ruti-kohen <131293568+ruti-kohen@users.noreply.github.com>
2023-12-15 01:33:20 +0200

    Update Candidate.cs

--- Server/Entities/Models/Candidate.cs
+++ Server/Entities/Models/Candidate.cs
@@ -1,23 +1,30 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
+using Server.Entities.Models;
 using System.Net;
 using System.Text.Json.Nodes;
 
 namespace Server.Entities.Models
 {
+    public class MyApiResponse
+    {
+    //נסיון להזרת הנתונים כאובייקט 
+        public List<Candidate> Candidates { get; set; }
+        public List<Language> Languages { get; set; }
+    }
+
     public class Candidate
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string BeginYear { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public List<int> Languages { get; set; }
+        //public List<Language> Languages { get; set; }
+    }
+
+    public class Language
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime? StartYear { get; set; }
-        public DateTime DateOfUpdatingTheCandidateInformation { get; set; }
-  
-        public TechnologicalLanguages[] LanguagesArr { get; set; }
-        
-
-    }
-    public class TechnologicalLanguages
-    {
-        public int Id { get; set; }
-        public string LanguagesName { get; set; }
     }
 }
